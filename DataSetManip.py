9cc9841ce6977f0eed2bebf84efcf8ce4232f0e5
Joshua Freedman <jfreedman0212@gmail.com>
2018-03-15 12:06:23 -0400

    updating on local computer to move to grid

--- DataSetManip.py
+++ DataSetManip.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
-# put big comment block here with description and usage of the program
+###############################################################################
+# DataSetManip.py - A command line program that manipulates the DataSet table #
+#		    Written by Joshua Freedman				      #
+#									      #
+# Usages: ./DataSetManip.py <flags>					      #
+#	-s <pathToDBFile>: Sets the env var to the specified path	      #
+#	-c <NickName> <DataTypeName> <Revision>				      #
+#		<RunPeriodName> <SoftwareVersionName>			      #
+#		<JanaConfigName> <JanaCalibContextValue>: adds these values   #
+#	-c <pathToFile>: adds all of the DataSets specified in the file      #
+#			  in the same format as the -a command		      #
+#	-
 
 # import dependencies
 import os
@@ -18,7 +29,7 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # argparse setup
-#parser = argparse.ArgumentParser(description='Changes/Retrieves DataSets')
-#parser.add_argument()
+parser = argparse.ArgumentParser(description='Changes/Retrieves DataSets')
+parser.add_argument()
 #put arguments here
-#args = parser.parse_args()
+args = parser.parse_args()
