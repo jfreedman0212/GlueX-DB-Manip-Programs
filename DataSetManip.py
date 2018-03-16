32dadec3bf9ecc7aec1a615fbb64c44efdbaa319
Joshua Freedman <jfreedman0212@gmail.com>
2018-03-16 02:33:40 -0400

    set up the template for the DataSetManip.py

--- DataSetManip.py
+++ DataSetManip.py
@@ -2,16 +2,10 @@
 
 ###############################################################################
 # DataSetManip.py - A command line program that manipulates the DataSet table #
-#		    Written by Joshua Freedman				      #
-#									      #
-# Usages: ./DataSetManip.py <flags>					      #
-#	-s <pathToDBFile>: Sets the env var to the specified path	      #
-#	-c <NickName> <DataTypeName> <Revision>				      #
-#		<RunPeriodName> <SoftwareVersionName>			      #
-#		<JanaConfigName> <JanaCalibContextValue>: adds these values   #
-#	-c <pathToFile>: adds all of the DataSets specified in the file      #
-#			  in the same format as the -a command		      #
-#	-
+#		    that also updates a webpage every time a new DataSet is   #
+#		    created.						      #
+# Written by Joshua Freedman						      #
+###############################################################################
 
 # import dependencies
 import os
@@ -29,7 +23,34 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # argparse setup
-parser = argparse.ArgumentParser(description='Changes/Retrieves DataSets')
-parser.add_argument()
-#put arguments here
+parser = argparse.ArgumentParser(description='Creates/Lists DataSets')
+parser.add_argument('-s',metavar='pathToDB',help='changes the environment variable for the DB file to the specified pathname')
+parser.add_argument('-c',metavar=('nickName','dataType','revision','runPeriod','softwareVersion','janaConfig','janaCalibContext'),\
+		    nargs=7,help='creates a new DataSet with the specified values for each attribute of it')
+parser.add_argument('-f',metavar='pathToFile',help='creates several DataSets from the specified file (file should be in format of -c flag)')
+parser.add_argument('-l','--list',action='store_true',help='lists all of the DataSets in a user-friendly format')
 args = parser.parse_args()
+
+# runs the modification procedures first, then the display procedure 
+# if a modification and a display flag are both called
+
+# procedures for the -s (set env var) flag
+if args.s is not None:
+	pass
+
+# procedures for the -c (create DataSet) flag
+if args.c is not None:
+	pass
+
+# procedures for the -f (create DataSets from file) flag
+if args.f is not None:
+	pass
+
+# runs the commit function here, as the modifications are done
+session.commit()
+
+#procedures for the -l,--list flag
+if args.list:
+	pass
+
+session.close()
