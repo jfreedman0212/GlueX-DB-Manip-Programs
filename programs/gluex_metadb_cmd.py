ed59be29c6691f8568dd5d13a7aa551c2c21e141
Joshua Freedman <jfreedman0212@gmail.com>
2018-05-21 16:41:13 -0400

    added the add datasets from file capability

--- programs/gluex_metadb_cmd.py
+++ programs/gluex_metadb_cmd.py
@@ -11,13 +11,41 @@ import DatabaseConnection as DBC
 import argparse
 import sys
 
+### useful variables and functions ###
+
 # generating a tuple to be used by the 'metavar' attribute of the create argument,
 # will also be used in the actual create process
 attributes = [a for a in dir(DBC.gluex_md.DataSet()) if not a.startswith('_') and a is not 'id'\
 	      and not callable(getattr(DBC.gluex_md.DataSet(),a)) and a is not 'metadata' and 'Id' not in a]
+# used to determine if an attribute is a reference to another table
 tables = [item for item in dir(DBC.gluex_md) if not item.startswith('_') \
 	  and 'DeclarativeMeta' in type(getattr(DBC.gluex_md,item)).__name__ \
 	  and item is not 'DataSet' and item is not 'Base']
+
+# function that creates a DataSet, used in create from command line and from file
+def createDataSet(dbc,arguments):
+	# dictionary that will contain the data needed to create the specified object
+	addedAttrs = {}
+
+	# generates the dictionary with corresponding attributes
+	for attr,arg in zip(attributes,arguments):
+		if attr not in tables:
+			addedAttrs[attr] = arg
+		else:
+			entry = None
+			try:
+				entry = dbc.search(attr,'name',arg)
+			except AttributeError:
+				entry = dbc.search(attr,'value',arg)
+			
+			if len(entry) > 0:
+				entryId = entry[0].id
+				addedAttrs[attr + 'Id'] = entryId
+	try:
+		dbc.create('DataSet',addedAttrs)
+	except AttributeError as exc:
+		print 'Something went wrong internally. Here is the error message:'
+		print exc
 
 # argparse setup
 parser = argparse.ArgumentParser(description='Creates/Lists DataSets')
@@ -25,7 +53,6 @@ parser.add_argument('-s',metavar='DB_URL',help='uses the specified URL instead o
 parser.add_argument('-c',metavar=tuple(attributes),nargs=len(attributes),help='creates a new DataSet with the specified values for each attribute of it')
 parser.add_argument('-f',metavar='pathToFile',help='creates several DataSets from the specified file (file should be in format of -c flag)')
 parser.add_argument('-l','--list',action='store_true',help='lists all of the DataSets in a user-friendly format')
-parser.add_argument('-v','--verbose',action='store_true',help='makes output more descriptive')
 args = parser.parse_args()
 
 # the DatabaseConnection object, will interact with the database
@@ -43,31 +70,33 @@ except DBC.InvalidDatabaseURLException as exc:
 
 # procedures for the -c (create DataSet) flag
 if args.c is not None:
-	# dictionary that will contain the data needed to create the specified object
-	addedAttrs = {}
-
-	# generates the dictionary with corresponding attributes
-	for attr,arg in zip(attributes,args.c):
-		if attr not in tables:
-			addedAttrs[attr] = arg
-		else:
-			entry = None
-			try:
-				entry = db.search(attr,'name',arg)
-			except AttributeError:
-				entry = db.search(attr,'value',arg)
-		
-			entryId = entry[0].id
-			addedAttrs[attr + 'Id'] = entryId
-	try:
-		db.create('DataSet',addedAttrs)
-	except AttributeError as exc:
-		print 'Something went wrong internally. Here is the error message:'
-		print exc
+	createDataSet(db,args.c)
 
 # procedures for the -f (create DataSets from file) flag
 if args.f is not None:
-	pass
+	txt_file = open(args.f,'r')
+	for line in txt_file.readlines():
+		arguments = line.split(' ')
+		# this for loop allows for entries to be specified with spaces as long as they
+		# are surrounded by double quotes (")
+		for string in arguments:
+			if string.startswith('\"'):
+				start = arguments.index(string)
+				i = start + 1
+				while True:
+					# could be cleaned up, but works for now
+					if not arguments[i].endswith('\"'):
+						arguments[start] = arguments[start] + ' ' + arguments[i]
+						arguments.pop(i)
+					else:
+						arguments[start] = arguments[start] + ' ' + arguments[i]
+						arguments.pop(i)
+						break
+				arguments[start] = arguments[start].replace('\"','')
+			if '\n' in string:
+				arguments[arguments.index(string)] = arguments[arguments.index(string)].replace('\n','')
+		createDataSet(db,arguments)
+	txt_file.close()
 
 #procedures for the -l,--list flag
 if args.list:
