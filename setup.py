617b4667d4d9755c4361490a93d9d6e63296191b
Joshua Freedman <jfreedman0212@gmail.com>
2018-07-17 12:57:00 -0400

    updated setup.py to include more info

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+	long_desc = f.read()
+
 setup(
+	name='gluex_metadb_programs',
+	description='Some programs that utilize the GlueX metadata database',
+	long_description = long_desc,
+	long_description_content_type='text/markdown',
+	author='Joshua Freedman',
+	author_email='jfreedman0212@gmail.com',
+	url='https://github.com/jfreedman0212/GlueX-DB-Manip-Programs',
 	scripts=[
 		'programs/query.py',
 		'programs/datasets_webpage.py',
@@ -10,5 +20,8 @@ setup(
 		'sqlalchemy',
 		'alembic',
 		'cherrypy'
-	]
+	],
+	classifiers=(
+		'Programming Language :: Python :: 2.7',
+	)
 )
